de891b184fefad67581973e42ccbc2115865dc89
Ian Cao <65691790+Enginma@users.noreply.github.com>
2023-07-19 11:56:36 -0400

    Part 2 code optimization and update

--- part2.cpp
+++ part2.cpp
@@ -1,87 +1,39 @@
 #include <iostream>
-#include <vector>
+#include <unordered_map>
 #include "part2_disjointSets.hpp"
 
 using namespace std;
 
-int main()
-{
-  vector<int> a;
-  int choice;
-  int element;
-  int set1, set2;
 
-  //main menu prompts
-  cout << "Menu:" << endl;
-  cout << "1: Make a new set" << endl;
-  cout << "2: Find an element" << endl;
-  cout << "3: Union two sets" << endl;
-  cout << "4: Quit" << endl;
 
-  //main loop for input and output
-  while( choice != 4 )
-    {
-      cout << "Enter your choice" << endl;
-      cin >> choice;
+int main(){
+    
+    // Main menu prompts. 
+    string choice;
 
-      if (cin.fail()) 
-      {
-            cout << "Invalid input. Please enter 1-4 as choice." << endl;
-            break;
-      }
-
-      switch (choice)
+    // Main loop for input and output. 
+    while(true){
+        cout << "Menu:" << endl;
+        cout << "1: Make a new integer disjoint set" << endl;
+        cout << "2: Make a new character disjoint set" << endl;
+        cout << "3: Quit" << endl;
+        cout << "Please enter your choice: ";
+        cin >> choice;
+        switch (choice[0])
         {
-          case 1: 
-            cout << "Enter new element greater than 0" << endl;
-            cin >> element;
-            if( element > 0)
-            {
-              makeset( element, a );
-              cout << "Operation complete" << endl;
-              break;
-            }
-            else
-            {
-              cout << "Please enter an element greater than 0" << endl;
-              break;
-            }
-          case 2:
-            cout << "Enter element to be found" << endl;
-            cin >> element;
-            if( element < 0 || element >= a.size() || find( element, a ) == -2 )
-            {
-              cout << "Entered element is not found" << endl;
-              break;
-            }
-            else
-            {
-              cout << "Your element is in set: " << find( element, a ) << endl;
-              break;
-            }
-          case 3:
-            cout << "Enter two sets to be unioned. Please press enter after each set" << endl;
-            cin >> set1;
-            cin >> set2;            
-            if( set1 < 0 || set1 >= a.size() || a[set1] == 0 || set2 < 0 || set2 >= a.size() || a[set2] == 0)
-            {
-              cout << "Entered set is not found" << endl;
-              break;
-            }
-            else
-            {
-              unionSet( set1, set2, a );
-              cout << "Operation complete" << endl;
-              break;
-            }
-          case 4:
+        case '1':
+            makeIntSet();
+            break;
+        case '2':
+            makeCharSet();
+            break;
+        case '3':
             cout << "Thanks for using our program!" << endl;
+            return 0;
+        default:   
+            cout << "Invalid input, please try again." << endl;
             break;
-          default:
-            cout << "Invalid choice. Please press 1-4" << endl;
-            break;
-        };
-    };
-  
-  return 0;  
-};
+        }        
+    }
+    return 0;
+}
--- part2_disjointSets.hpp
+++ part2_disjointSets.hpp
@@ -1,85 +1,201 @@
 #ifndef DISJOINTSETS_H
 #define DISJOINTSETS_H
 
-using namespace std;
-
+#include <iostream>
+#include <unordered_map>
 #include <vector>
 
-//internal method for checking the heights of sets
-int height( int set, vector<int> & a )
-{
-  int max = 0, tmpMax = 0, tmp;
-  
-  for( int i = 0; i < a.size(); ++i )
-    {
-      if( a[i] == set && a[i] == i )
-        continue;
-      else if( a[i] == set )
-      {
-        tmpMax = 0;
-        tmpMax++;
-        for( int j = 0; j < a.size(); ++j )
-          {
-            if( a[j] == i )
-            {
-              tmp = j;
-              j = 0;
-              tmpMax++;
-            }
-          }
-      }
-      
-      if( tmpMax > max)
-        max = tmpMax;
+using namespace std;
+
+// The Disjointset class. We have a node struct for binary tree implementation. 
+// We use unordered map library to make hash table implementation.
+
+template <typename T>
+class DisjointSet {
+private:
+    struct Node {
+        T element;
+        int rank;
+        Node* parent;
+    };
+
+    unordered_map<T, Node*> nodes;
+
+
+// Function to find the root node for other functions. 
+    Node* findRoot(Node* node) {
+        while (node->parent != node) {
+            node = node->parent;
+        }
+        return node;
     }
-  return max;
+
+public:
+    DisjointSet() {}
+
+// The make set function in Dijkstra's algorithm. 
+
+    void makeSet(T element) {
+        if (nodes.find(element) == nodes.end()) {
+            Node* newNode = new Node{element, 0, nullptr};
+            newNode->parent = newNode;
+            nodes[element] = newNode;
+        }
+    }
+
+// The find set function in Dijkstra's algorithm. 
+
+    T find(T element) {
+        if (nodes.find(element) == nodes.end()) {
+            cerr << "Element not found." << endl;
+            return T{};
+        }
+
+        Node* node = nodes[element];
+        Node* root = findRoot(node);
+
+        while (node != root) {
+            Node* next = node->parent;
+            node->parent = root;
+            node = next;
+        }
+
+        return root->element;
+    }
+
+// The union set function in Dijkstra's algorithm. 
+
+    void unionSets(T element1, T element2) {
+        Node* node1 = nodes[element1];
+        Node* node2 = nodes[element2];
+
+        if (!node1 || !node2) {
+            cerr << "No two elements present." << endl;
+            return;
+        }
+
+        Node* root1 = findRoot(node1);
+        Node* root2 = findRoot(node2);
+
+        if (root1 == root2) {
+            return; 
+        }
+
+        // Union by rank to keep the tree balanced.
+        if (root1->rank < root2->rank) {
+            root1->parent = root2;
+        } else if (root1->rank > root2->rank) {
+            root2->parent = root1;
+        } else {
+            root1->parent = root2;
+            root2->rank++;
+        }
+    }
+
+  // Deconstructor. 
+    ~DisjointSet() {
+        for (auto it = nodes.begin(); it != nodes.end(); ++it) {
+            delete it->second;
+        }
+    }
 };
 
-//finds an element and returns what set it is in
-int find( int element, vector<int> & a )
-{
-  int f = -2;
-  if( a[element] == 0 )
-  {
-    return f;
-  }
-  else if( element == a[element] )
-  {
-    return element;
-  }
-  else
-  {
-    a[element] = find( a[element], a );
-      return a[element];
-  }
-};
 
-//merges two sets based on height to keep the tree as short as possible
-void unionSet( int set1, int set2, vector<int> & a )
-{
-  if( height( set1, a ) >= height( set2, a ) )
-  {
-    a[set2] = set1;
-    return;
-  }
-  else
-  {
-    a[set1] = set2;
-    return;
-  }
-};
+// Making an integer set. 
 
-//makes a new set hashed to give the int input its equivalent index number
-int makeset( int element, vector<int> & a )
-{
-  int i = element;
-  if( element >= a.size() )
-  {
-    a.resize( element + 1 );
-    return a[i] = i;
-  }
-  else
-    return a[i] = i;
-};
+void makeIntSet(){
+    DisjointSet<int> number;
+    cout << "---------------------------------------------" << endl;
+    cout << "Empty integer disjoint set created!" << endl;
+    cout << "Please choose one of the following options:" << endl;
+    cout << "1. make a set with given element." << endl;
+    cout << "2. Union two sets." << endl;
+    cout << "3. Find an element." << endl;
+    cout << "4. Quit building." << endl;
+    string s;
+    int input, set1, set2;
+    while(true){
+        cout << "Please enter your choice: ";
+        cin >> s;
+        switch (s[0])
+        {
+        case '1':
+            cout << "Please enter an integer to create a set: ";
+            cin >> input; 
+            number.makeSet(input);
+            break;
+        
+        case '2':
+            cout << "Please enter set 1: ";
+            cin >> set2;
+            cout << "Please enter set 2: ";
+            cin >> set1;
+            number.unionSets(set1, set2);
+            break;
+        
+        case '3':
+            cout << "Please enter an integer to perform find: ";
+            cin >> input;
+            cout << "The representation of " << input << " is: " << number.find(input) << "." << endl;
+            break;
+
+        case '4':
+            cout << endl;
+            return;
+        default:
+            cout << "Invalid input, please try again." << endl;
+            break;
+        }
+    }
+}
+
+
+// Make character set. This is for taking care of character inputs. 
+
+void makeCharSet(){
+    DisjointSet<char> character;
+    cout << "---------------------------------------------" << endl;
+    cout<< "Empty character disjoint set created!" << endl;
+    cout << "Please choose one of the following options:" << endl;
+    cout << "1. Make a set with given element." << endl;
+    cout << "2. Union two sets." << endl;
+    cout << "3. Find an element in the sets." << endl;
+    cout << "4. Quit building." << endl;
+    string s;
+    char input, set1, set2;
+    while(true){
+        cout << "Please enter your choice: ";
+        cin >> s;
+        switch (s[0])
+        {
+        case '1':
+            cout << "Please enter a character to create a set: ";
+            cin >> input; 
+            character.makeSet(input);
+            break;
+        
+        case '2':
+            cout << "Please enter set 1: ";
+            cin >> set2;
+            cout << "Please enter set 2: ";
+            cin >> set1;
+            character.unionSets(set1, set2);
+            break;
+        
+        case '3':
+            cout << "Please enter an integer to perform find: ";
+            cin >> input;
+            cout << "The representation of " << input << ": " << character.find(input) << "." << endl;
+            break;
+
+        case '4':
+            cout << endl;
+            return;
+        default:
+            cout << "Invalid input, please try again." << endl;
+            break;
+        }
+    }
+}
 
 #endif
